4965970bae8a6d131f9573b1b78f56f053fac73f
ahmedherhesh <ahmedherhesh3@gmail.com>
2021-10-17 00:39:01 +0200

    auth:api

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\Auth\LoginController;
 
 
-Route::middleware('auth:passport')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
